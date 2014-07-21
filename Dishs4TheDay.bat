f778544652d609d350b9b48147746be70e47659e
kuriyakose <kpchiramel@gmail.com>
2014-07-21 17:21:17 -0500

    add

---
+++ Dishs4TheDay.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe DishSelection/DishSelection.csproj /property:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe DishSelectionTest/DishSelectionTest.csproj /property:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Dishs4TheDay/Dishs4TheDay.csproj /property:Configuration=Release
